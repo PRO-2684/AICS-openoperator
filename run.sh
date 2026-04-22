19b165374e2abf8e62bea5c834e093c5e32b6a3d
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2026-04-22 22:17:05 +0800

    chore: Creating mlu files

--- run.sh
+++ run.sh
@@ -15,12 +15,16 @@ else
   TARGET="$MLU_SOURCE"
 fi
 
-cncc "${MLU_SOURCE}" \
-    -I /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include \
-    -I /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include/torch/csrc/api/include \
-    # -I /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include/TH \
-    # -I /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include/THC \
-    -o "target/${TARGET}" --bang-mlu-arch=mtp_592 -O3 -lm
-# mtp_372
+cncc -c "${MLU_SOURCE}" -o "target/${TARGET}" -DTORCH_EXTENSION_NAME=LeakyReLU -DTORCH_API_INCLUDE_EXTENSION_H -DPYBIND11_COMPILER_TYPE=\"_gcc\" -DPYBIND11_STDLIB=\"_libstdcpp\" -DPYBIND11_BUILD_ABI=\"_cxxabi1011\" \
+    -isystem /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include \
+    -isystem /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include/torch/csrc/api/include \
+    -isystem /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include/TH \
+    -isystem /torch/venv3/pytorch/lib/python3.10/site-packages/torch/include/THC \
+    -isystem /torch/venv3/pytorch/lib/python3.10/site-packages/torch_mlu/csrc \
+    -isystem /torch/venv3/pytorch/lib/python3.10/site-packages/torch_mlu/csrc/api/include/torch_mlu \
+    -isystem /usr/local/neuware/include -isystem /opt/py3.10/include/python3.10 \
+    -D_GLIBCXX_USE_CXX11_ABI=0 --no-neuware-version-check \
+    --bang-mlu-arch=mtp_372 \
+    -fPIC --neuware-path=/usr/local/neuware -std=c++17
 
 target/${TARGET}
